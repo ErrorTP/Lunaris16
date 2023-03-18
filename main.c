a3e64bfddf57e157eae9d6d58983e06a7e22d72f
ErrorTP <57261793+ErrorTheProgrammer@users.noreply.github.com>
2023-03-18 22:31:27 +0000

    Cleaned some code up

--- main.c
+++ main.c
@@ -152,11 +152,10 @@ int main(int argc, char *argv[]) {
     printf("Usage: %s input_file [output_file]\n", argv[0]);
     return 0;
   }
+  printf("Lunaris and Horizon16 are in early development!\nLunaris for the Horizon16 Version 1.0.0\n")
   #ifdef __linux__
-  printf("Lunaris for the Horizon16 Version 1.0.0\n");
   printf("Currently running on Linux\n");
   #else
-  printf("Lunaris for the Horizon16 Version 1.0.0\n");
   printf("Running on an unsupported operating system!\nBeware of bugs!\n")
   #endif
   FILE *input_file = fopen(argv[1], "r");
